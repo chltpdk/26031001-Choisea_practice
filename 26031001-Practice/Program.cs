c24f7f1b56d6c8b4bac330e539c278a4cf3d5271
chltpdk <chltpdk1029@gmail.com>
2026-08-26 12:57:38 +0900

    이력서 연습

--- 26031001-Practice/Program.cs
+++ 26031001-Practice/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("나의 이력서");
+            Console.ForegroundColor = ConsoleColor.White; ;
+            //Console.SetCursorPosition(200, 100);
+            Console.WriteLine("이름 : 최세아");
+            Console.WriteLine("특징 : 띨빵");
         }
     }
 }
