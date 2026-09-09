f3eb8dc60bfe722aa5e60d3c3493a252716d246e
chltpdk <chltpdk1029@gmail.com>
2026-09-09 12:21:41 +0900

    0909수업

---
+++ 26031001_0909_1.cs
@@ -0,0 +1,136 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //    Console.WriteLine("Hello, World!");
+
+        //System.Console.WriteLine(sizeof(byte));  // 중요 ㄴ
+        //System.Console.WriteLine(sizeof(char));
+        //System.Console.WriteLine(sizeof(short)); // 중요 ㄴ
+        //System.Console.WriteLine(sizeof(int));
+        //System.Console.WriteLine(sizeof(long));
+        //System.Console.WriteLine(sizeof(float));
+        //System.Console.WriteLine(sizeof(double));
+
+        //char value1 = 'a';
+        //char value2 = 'A';
+        //System.Console.WriteLine(value1 + value2);
+        //System.Console.WriteLine(value1 - value2);
+        //System.Console.WriteLine(value1 * value2);
+        //System.Console.WriteLine(value1 / value2);
+        //System.Console.WriteLine(value1 % value2);
+
+        //    char value1 = 'a';
+        //    char value2 = '0';
+
+        //System.Console.WriteLine(sizeof(char));
+        //System.Console.WriteLine(sizeof(short)); // 중요 ㄴ
+        //System.Console.WriteLine(sizeof(int));
+        //System.Console.WriteLine(sizeof(long));
+        //System.Console.WriteLine(sizeof(float));
+        //System.Console.WriteLine(sizeof(double));
+
+        //    string message1 = "안녕하세요 방가방가루.";
+        //    string message2 = "abcde. fghij.";
+
+        //    Console.WriteLine(message1[0]);
+        //    Console.WriteLine(message1[1]);
+        //    Console.WriteLine(message1[2]);
+        //    Console.WriteLine("안녕하세요 방가방가루"[2]);
+        //    Console.WriteLine(message1[2]);
+        //    Console.WriteLine(message1.Length);
+
+        //    int va1; // System int32
+        //    float va2; // System. Single va2;
+
+        //int value3 = 100;
+        //value3 += 200; Console.WriteLine(value3);
+        //value3 -= 200; Console.WriteLine(value3);
+        //value3 *= 200; Console.WriteLine(value3);
+        //value3 /= 200; Console.WriteLine(value3);
+        //value3 %= 200; Console.WriteLine(value3);
+
+        //////////////////////////////////////////////////////////
+
+        //string m1 = "Hello";
+        ////m1 = m1 + "World"; Console.WriteLine(m1);
+        //m1 += " World";
+        //m1 = string.Concat(m1,"  Welcome !!");
+        //Console.WriteLine(m1);
+
+        //string resDir = "resource/";
+        //string textureFile = "m1.png";
+
+        //string texPath = resDir + textureFile;
+        //Console.WriteLine(texPath);
+
+
+        //////////////////////////////////////////////
+        // 단항 연산자 (어떤 변수가 있다면
+        //int value = 100;
+        //int result = 0;
+
+        //result = ++value; 
+        //Console.WriteLine($"result: {result} value: {value}"); // value += 1;
+
+        //result = value++; 
+        //Console.WriteLine($"result: {result} value: {value}");// value += 1;
+
+        // // value++;
+        //// result = value;
+        //// value = value+1;
+
+
+        //result = --value; 
+        //Console.WriteLine($"result: {result} value: {value}");// value -= 1;
+
+        //result = value--; 
+        //Console.WriteLine($"result: {result} value: {value}");// value -= 1;
+
+        // value--;
+        // result = value;
+        // value = value-1;
+
+
+        //////////////////////////////////
+
+        /// GetType
+        /// 
+        //Console.WriteLine(10.GetType());
+        //Console.WriteLine(10.0.GetType());
+        //Console.WriteLine(10.0f.GetType());  // 해상도 = 정밀도 = 싱글
+        //Console.WriteLine('한'.GetType());
+        //Console.WriteLine("message".GetType());
+
+        //////////////////////////////////////////
+
+
+        //var value = "하이하이루";
+        //Console.WriteLine("value Type : " + value.GetType());
+
+
+        //var vlaue = 0; Console.WriteLine($"value type : { vlaue.GetType()}");
+        //var vlaue2 = 0.0; Console.WriteLine($"value type : { vlaue2.GetType()}");
+        //var vlaue3 = 0.0f; Console.WriteLine($"value type : { vlaue3.GetType()}");
+        //var vlaue4 = '가'; Console.WriteLine($"value type : { vlaue4.GetType()}");
+        //var vlaue5 = "문자열"; Console.WriteLine($"value type : { vlaue5.GetType()}");
+        //var vlaue6 = vlaue3; Console.WriteLine($"value type : { vlaue6.GetType()}");
+        //var vlaue7 = 0L; Console.WriteLine($"value type : {vlaue7.GetType()}");
+
+        //int vlaue = 0; Console.WriteLine($"value type : {vlaue.GetType()}");
+        //double vlaue2 = 0.0; Console.WriteLine($"value type : {vlaue2.GetType()}");
+        //float vlaue3 = 0.0f; Console.WriteLine($"value type : {vlaue3.GetType()}");
+        //char vlaue4 = '가'; Console.WriteLine($"value type : {vlaue4.GetType()}");
+        //string vlaue5 = "문자열"; Console.WriteLine($"value type : {vlaue5.GetType()}");
+        //float vlaue6 = vlaue3; Console.WriteLine($"value type : {vlaue6.GetType()}");
+        //long vlaue7 = 0L; Console.WriteLine($"value type : {vlaue7.GetType()}");
+
+
+        ////////////////////////////////////////////////////////////////
+
+
+
+
+    }
+}
+
