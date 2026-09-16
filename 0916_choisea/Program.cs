27b50c9d2770a0eedf6547dcf47cea73a76e8e96
chltpdk <chltpdk1029@gmail.com>
2026-09-16 14:32:27 +0900

    0916과제

---
+++ 0916_choisea/Program.cs
@@ -0,0 +1,120 @@
+﻿
+using System;
+class Program
+{
+    //9번 문제//
+    //static void Main()
+    //{
+    //Console.WriteLine("태어난 연도를 입력하세요 : ");
+    //int year = int.Parse(Console.ReadLine());
+    //int remainder = year % 12;
+
+    //string zodiac = "";
+
+    //switch (remainder)
+    //{
+    //    case 0:
+    //        zodiac = "원숭이띠"; break;
+    //    case 1:
+    //        zodiac = "닭띠"; break ;
+    //    case 2:
+    //        zodiac = "개띠"; break;
+    //    case 3:
+    //        zodiac = "돼지띠"; break ;
+    //    case 4:
+    //        zodiac = "쥐띠"; break ;
+    //    case 5:
+    //        zodiac = "소띠"; break;
+    //    case 6:
+    //        zodiac = "범띠"; break;
+    //    case 7:
+    //        zodiac = "토끼띠"; break;
+    //    case 8:
+    //        zodiac = "용띠"; break;
+    //    case 9:
+    //        zodiac = "뱀띠"; break;
+    //    case 10:
+    //        zodiac = "말띠"; break;
+    //    case 11:
+    //        zodiac = "양띠"; break;
+    //}
+
+    //Console.WriteLine($"{year}년은 {zodiac}입니다.");
+    //}
+
+    // 10번 문제 //
+    //    static void Main()
+    //{
+    //    Console.WriteLine("현재 월을 입력하세요 : ");
+    //    int month = int.Parse(Console.ReadLine());
+
+    //    if (month == 12 || month == 1 || month == 2)
+    //    {
+    //        Console.WriteLine($"{month}월은 겨울입니다");
+    //    }
+    //    else if (month >=3 || month <= 5)
+    //    {
+    //        Console.WriteLine($"{month}월은 봄 입니다.");
+    //    }
+    //    else if (month >=6 || month <= 8)
+    //    {
+    //        Console.WriteLine($"{month}월은 여름입니다.");
+    //    }
+    //    else if (month >=9 || month <= 11)
+    //    {
+    //        Console.WriteLine($"{month}월은 가을입니다");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("잘못된 입력입니다. 1 ~ 12사이의 숫자를 입력해 주세요");
+    //    }
+
+
+    // 11번 문제 //
+    //if (x > 10 && x< 20)
+    //  {
+    //   Console.WriteLine("조건에 맞습니다");
+    //  }
+
+
+    // 12번 문제 //
+    //1번 : 공백 출력
+    //2번 : 츨력 없음 
+    //3번 : 100
+
+
+    //13번 문제 //
+    //int x = 10;
+    //string result = (x % 2 == 0) ? "짝수" : "홀수";
+    //Console.WriteLine(result);
+
+
+    //14번 문제 //
+    //static void Main(string[] args)
+    //{
+    //    Console.Write("학년을 입력하세요: ");
+    //    int level = int.Parse(Console.ReadLine());
+
+    //    switch (level)
+    //    {
+    //        case 1:
+    //            Console.WriteLine("수강해야 하는 전공 학점: 12학점");
+    //            break;
+    //        case 2:
+    //            Console.WriteLine("수강해야 하는 전공 학점: 18학점");
+    //            break;
+    //        case 3:
+    //            Console.WriteLine("수강해야 하는 전공 학점: 10학점");
+    //            break;
+    //        case 4:
+    //            Console.WriteLine("수강해야 하는 전공 학점: 18학점");
+    //            break;
+    //        default:
+    //            Console.WriteLine("잘못된 입력입니다.");
+    //            break;
+    //    }
+    //}
+}
+}
+
+
